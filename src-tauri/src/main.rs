52d9dba4e7c8d5405f4cd6477689cd97f4513f0f
dxxxxy <introsdream@gmail.com>
2023-10-01 00:48:45 -0400

    feat: create presence tauri command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,24 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::{thread, time::Duration};
-
+use discord_presence::Client;
 use tauri::Manager;
+use lazy_static::lazy_static;
+
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref DISCORD_CLIENT: Mutex<Client> = Mutex::new(Client::new(1156699732765310976));
+}
+
+#[tauri::command]
+fn presence(playing: bool, title: &str, author: &str, artwork: &str) {
+    let client = &DISCORD_CLIENT;
+    let mut client = client.lock().unwrap();
+
+    client.set_activity(|act| act.details(title).state(author).assets(|ass| ass.large_image(artwork))).unwrap();
+}
+
 
 fn main() {
     tauri::Builder::default()
@@ -13,11 +29,14 @@ fn main() {
                 loop {
                     thread::sleep(Duration::from_secs(1));
                     window.eval("console.log('Hello from Rust')").unwrap();
+
+                    //get state from window
                 }
             });
 
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![presence])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
